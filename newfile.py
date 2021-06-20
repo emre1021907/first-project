cb5d075198c001bb6c341a65f81f9efcc8f7f594
emre köse <koseemre2@gmail.com>
2021-06-20 20:02:56 +0300

    new git query

--- newfile.py
+++ newfile.py
@@ -4,4 +4,7 @@ print("new file has been added")
 # git add 
 # git commit
 # git push
-# branching
+# git branch --> render list of branches  
+# git checkout -b feature-emre  --> creates a branch
+# git checkout branch name --> switches to the branch
+# git branch -d branchname  --> deletes the branch
